d9bc52b256599c4e02059ac715d4550194749421
Michael B. Syson <masarapmabuhay@gmail.com>
2019-07-08 20:35:36 +0800

    +added: history folder +added: previously used set of instructions in the history folder +updated: instructions to automatically encrypt HTML input files --> work-in-progress

--- Cryptography/OpenMonthlyPaymentSummaryReport.bat
+++ Cryptography/OpenMonthlyPaymentSummaryReport.bat
@@ -3,16 +3,20 @@ echo //-----------------------------------------------------------------------
 echo //  Marikina Orthopedic Specialy Clinic - Sta. Lucia Health Care Centre
 echo //  Usbong Social Systems, Inc. (USBONG.PH)
 echo //
-echo //  Monthly Payment Summary Report (2018)
+echo //  Monthly Payment Summary Report
 echo //-----------------------------------------------------------------------
 echo.
 set mainDirectory=%CD%
 set /p pass="Enter passphrase:"
 echo %pass% > library/pass.bin
 cd library
-openssl enc -d -pbkdf2 -aes256 -in %mainDirectory%/input/EncryptedMonthlyPaymentSummaryTreatment.html -out %mainDirectory%/output/DecryptedMonthlyPaymentSummaryTreatment.html -kfile pass.bin
-openssl enc -d -pbkdf2 -aes256 -in %mainDirectory%/input/EncryptedMonthlyPaymentSummaryConsultation.html -out %mainDirectory%/output/DecryptedMonthlyPaymentSummaryConsultation.html -kfile pass.bin
+openssl enc -d -pbkdf2 -aes256 -in %mainDirectory%/input/EncryptedMonthlyPaymentSummaryTreatment2018.html -out %mainDirectory%/output/DecryptedMonthlyPaymentSummaryTreatment2018.html -kfile pass.bin
+openssl enc -d -pbkdf2 -aes256 -in %mainDirectory%/input/EncryptedMonthlyPaymentSummaryConsultation2018.html -out %mainDirectory%/output/DecryptedMonthlyPaymentSummaryConsultation2018.html -kfile pass.bin
+openssl enc -d -pbkdf2 -aes256 -in %mainDirectory%/input/EncryptedMonthlyPaymentSummaryTreatment2019.html -out %mainDirectory%/output/DecryptedMonthlyPaymentSummaryTreatment2019.html -kfile pass.bin
+openssl enc -d -pbkdf2 -aes256 -in %mainDirectory%/input/EncryptedMonthlyPaymentSummaryConsultation2019.html -out %mainDirectory%/output/DecryptedMonthlyPaymentSummaryConsultation2019.html -kfile pass.bin
 del pass.bin
 cd ..
-explorer file:///%CD%/output/DecryptedMonthlyPaymentSummaryTreatment.html
-explorer file:///%CD%/output/DecryptedMonthlyPaymentSummaryConsultation.html
+explorer file:///%CD%/output/DecryptedMonthlyPaymentSummaryTreatment2018.html
+explorer file:///%CD%/output/DecryptedMonthlyPaymentSummaryConsultation2018.html
+explorer file:///%CD%/output/DecryptedMonthlyPaymentSummaryTreatment2019.html
+explorer file:///%CD%/output/DecryptedMonthlyPaymentSummaryConsultation2019.html
