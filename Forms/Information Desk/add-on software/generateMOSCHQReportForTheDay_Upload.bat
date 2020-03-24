f3345b7a5775a012e931251f954fe8dff99b2018
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-03-24 18:00:05 +0800

    Add files via upload

--- Forms/Information Desk/add-on software/generateMOSCHQReportForTheDay_Upload.bat
+++ Forms/Information Desk/add-on software/generateMOSCHQReportForTheDay_Upload.bat
@@ -1,6 +1,6 @@
 pushd %~dp0
 
-java -cp .\software;.\software\org.json.jar;.\software\org.apache.httpclient.jar;.\software\org.apache.httpcore.jar;.\software\org.apache.commons-logging.jar UsbongHTTPConnect "upload" http://localhost/ "output/kms/"*.txt
+java -cp .\software;.\software\org.json.jar;.\software\org.apache.httpclient.jar;.\software\org.apache.httpcore.jar;.\software\org.apache.commons-logging.jar UsbongHTTPConnect "upload" http://mosc-accounting/ "output/kms/"*.txt
 
 popd
 
