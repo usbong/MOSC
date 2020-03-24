646b9c93ada7189bbf3761b1f280786e509ec472
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-03-24 17:50:47 +0800

    Add files via upload

--- Forms/Information Desk/add-on software/generateMOSCHQReportForTheDay_Upload.bat
+++ Forms/Information Desk/add-on software/generateMOSCHQReportForTheDay_Upload.bat
@@ -1,7 +1,8 @@
-set mainDirectory=G:\Usbong MOSC\Everyone\Information Desk\add-on software
-cd /d %mainDirectory%
+pushd %~dp0
+
 java -cp .\software;.\software\org.json.jar;.\software\org.apache.httpclient.jar;.\software\org.apache.httpcore.jar;.\software\org.apache.commons-logging.jar UsbongHTTPConnect "upload" http://localhost/ "output/kms/"*.txt
-cd ..
-%2
+
+popd
+
 PAUSE
 #exit
