cb0bf3b8e822acf2b86e886242f6a6b044795fdb
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-05-21 15:47:32 +0800

    Add files via upload

---
+++ Forms/Information Desk/viewAllReportsForTheDayUnified.bat
@@ -0,0 +1,9 @@
+"C:\Program Files\Mozilla Firefox\firefox.exe" "http://localhost/usbong_kms/index.php/report/viewReportMedicineUnified"
+
+"C:\Program Files\Mozilla Firefox\firefox.exe" "http://localhost/usbong_kms/index.php/report/viewReportMedicineAsteriskUnified"
+
+"C:\Program Files\Mozilla Firefox\firefox.exe" "http://localhost/usbong_kms/index.php/report/viewReportNonMedicineUnified"
+
+"C:\Program Files\Mozilla Firefox\firefox.exe" "http://localhost/usbong_kms/index.php/report/viewPayslipFor/Pedro"
+
+"C:\Program Files\Mozilla Firefox\firefox.exe" "http://localhost/usbong_kms/index.php/report/viewPayslipFor/Peter"
