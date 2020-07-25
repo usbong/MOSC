3c6f5c9e1d126bb2f4f9a1b9f5505e0b0e24d225
Masarap Mabuhay <masarapmabuhay@gmail.com>
2020-07-25 08:33:32 +0800

    Add files via upload

---
+++ Forms/Information Desk/unified.bat
@@ -0,0 +1,3 @@
+start cmd /c "G:\Usbong MOSC\Everyone\Information Desk\viewAllReportsForTheDayUnified.bat"
+start cmd /c "G:\Usbong MOSC\Everyone\Information Desk\moscReportForTheDay.xlsm"
+start cmd /c "C:\xampp\htdocs\usbong_kms\kasangkapan\phantomjs-2.1.1-windows\bin\autoScreenCaptureReports.bat"
